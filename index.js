64e155770a35383fb38cebc722554db9fcf8e7ee
MokuaEnock <enock.mokua@student.moringaschool.com>
2022-08-15 18:55:28 +0300

    complete assignment

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
+let num1 = 62
+let num2 = 1
 
-// Write your code here
+let multiply = num1 * num2
+
+let random = Math.floor(Math.random() * 10);
+
+let max = Math.max(3, 5, 20)
+let num3 = 9
+let num4 = 5
+let mod = (num3%num4)
